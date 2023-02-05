fef8ba08e91517a8799015a603f821743026f0fb
Максим Жерс <fiezzyy@MacBook-Pro-Maksim.local>
2023-02-05 19:42:26 +0300

    some fixes

--- src/ui/Checkbox/style.ts
+++ src/ui/Checkbox/style.ts
@@ -4,12 +4,12 @@ export const Wrapper = styled.div<{ isChecked?: boolean }>`
   width: 20px;
   height: 20px;
   border-radius: 50%;
-  background-color: ${({ theme, isChecked }) => (isChecked ? 'red' : 'white')};
+  background-color: ${({ theme, isChecked }) =>
+    isChecked ? theme.color.btn : theme.color.bgSecondary};
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1px solid red;
 
   svg {
     path {
@@ -17,6 +17,3 @@ export const Wrapper = styled.div<{ isChecked?: boolean }>`
     }
   }
 `
-
-//background-color: ${({ theme, isChecked }) =>
-//isChecked ? theme.color.btn : theme.color.bgSecondary};
